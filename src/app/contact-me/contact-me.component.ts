d94289d28b3f9ddefcd538a054d057a3fed19811
Ben <benjamin.tanone@gmail.com>
2018-09-30 23:38:14 +1000

    added disabled button css for primary buttons; finished the form; need to wire submission logic

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -12,14 +12,32 @@ export class ContactMeComponent implements OnInit {
 
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    message: new FormControl('', [Validators.required, Validators.maxLength(1000)])
+    message: new FormControl('', [Validators.required, Validators.maxLength(1000)]),
+    reason: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    lastName: new FormControl('', [Validators.required, Validators.maxLength(50)])
   });
 
   ngOnInit() {
   }
 
   get email() {
-    return this.form.get('email')
+    return this.form.get('email');
   }
 
+  get message() {
+    return this.form.get('message');
+  }
+
+  get reason() {
+    return this.form.get('reason');
+  }
+  
+  get firstName() {
+    return this.form.get('firstName');
+  }
+
+  get lastName() {
+    return this.form.get('lastName');
+  }
 }
